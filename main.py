96aba9f792517dc24d01f2339ab4d44bfeb0bbe7
raj89er <134079776+raj89er@users.noreply.github.com>
2024-03-25 11:40:28 -0400

    added dark blue background

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ pygame.init()
 # create the display, with a resolution of 800px height and 600px width
 screen = pygame.display.set_mode((800, 600))
 
-# add fuctionality to the close button
+# TItle and Icon
+pygame .display.set_caption("Space Invaders v 0.1")
+icon= pygame.image.load("./images/icon.png")
+pygame.display.set_icon(icon)
+
+# Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    #background fill in RGB
+    screen.fill((6,57,112))
+    pygame.display.update()
